24a70c175739929484fc6ef1154027d3b3c5241a
Odunosh Moses <Odunoshomoses@gmail.com>
2023-04-28 07:19:04 +0000

    fixing menu toggle later

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Education from './components/Education'
 import Experience from './components/Experience'
 import Contact from './components/Contact'
 import { useRef } from 'react'
+import {AiOutlineMenu} from 'react-icons/ai'
 
 function App() {
   let header = useRef<HTMLDivElement | null>(null)
@@ -24,6 +25,7 @@ function App() {
       <div className='w-[100%] lg:w-[20%]'>
       <div className='w-[100%] lg:w-[20%] lg:p-[10px] bg-[#181818] fixed lg:block justify-evenly py-[10px] px-[5px] top-0'>
         <h1 className='text-[#fff] font-bold lg:text-[2rem] cursor-pointer hidden lg:block' onClick={() => scrollToSection(header)}>Moscode</h1>
+        <p className="text-[#fff] font-bold lg:text-[2rem] cursor-pointer lg:hidden"></p>
         <div className='lg:h-[100vh] lg:flex lg:flex-col lg:justify-center justify-evenly hidden'>
                 <p className='text-[#fff] font-bold lg:text-[2rem] cursor-pointer lg:hidden' onClick={() => scrollToSection(header)}>MO.</p>
                 <p className='pl-[20px] lg:pl-[0px] text-[#4f4f4f] font-medium lg:text-[2rem] lg:pb-[2rem] cursor-pointer' onClick={() => scrollToSection(portfolio)}>Portfolio</p>
